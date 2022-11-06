2842b4d816e3b0747bf027ffbbde953e4f181611
keisukesanuki <n41210guitar@gmail.com>
2022-11-07 05:19:05 +0900

    modify

--- public/index.php
+++ public/index.php
@@ -1,2 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Flanker</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
 <?php
-echo "flanker";
+require('../lib/jmeter.php');
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (is_uploaded_file($_FILES['scenariofile']['tmp_name'])) {
+        $uploaddir = '../upload/';
+        $uploadfile = $uploaddir . basename($_FILES['scenariofile']['name']);
+        if (!move_uploaded_file($_FILES['']['tmp_name'], $uploadfile)) {
+            echo "File upload has failed.\n";
+        }
+    } else {
+        if (isset($_POST['posted'])){
+            $uploadfile = $_POST['posted'];
+        } else {
+            echo "File does not exist.\n";
+        }
+    }
+
+    echo "<div>";
+    echo "<form enctype=\"multipart/form-data\" action=\"\" method=\"POST\">";
+    echo "<input name=\"scenariofile\" type=\"file\" />";
+    echo "<input type=\"submit\" value=\"upload\" />";
+    echo "</form>";
+    echo "</div>";
+} else {
+    echo "<div>";
+    echo "<form enctype=\"multipart/form-data\" action=\"\" method=\"POST\">";
+    echo "<input name=\"scenariofile\" type=\"file\" />";
+    echo "<input type=\"submit\" value=\"upload\" />";
+    echo "</form>";
+    echo "</div>";   
+}
+?>
+
+</body>
+</html>

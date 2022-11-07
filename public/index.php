fcecab39c236e55c76296bd5711162b5115e5473
keisukesanuki <n41210guitar@gmail.com>
2022-11-07 21:13:01 +0900

    modify

--- public/index.php
+++ public/index.php
@@ -1,42 +1,34 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Flanker</title>
-  <link rel="stylesheet" href="style.css" type="text/css">
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Flanker</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body>
 
-<?php
-require('../lib/jmeter.php');
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (is_uploaded_file($_FILES['scenariofile']['tmp_name'])) {
-        $uploaddir = '../upload/';
-        $uploadfile = $uploaddir . basename($_FILES['scenariofile']['name']);
-        if (!move_uploaded_file($_FILES['scenariofile']['tmp_name'], $uploadfile)) {
-            echo "File upload has failed.\n";
+        <?php
+        require('../lib/jmeter.php');
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (is_uploaded_file($_FILES['scenariofile']['tmp_name'])) {
+                $uploaddir = '../upload/';
+                $uploadfile = $uploaddir . basename($_FILES['scenariofile']['name']);
+                if (!move_uploaded_file($_FILES['scenariofile']['tmp_name'], $uploadfile)) {
+                    echo "File upload has failed.\n";
+                }
+            } else {
+                //
+            }
         }
-    } else {
-        //
-    }
-    echo "<div>";
-    echo "<form enctype=\"multipart/form-data\" action=\"\" method=\"POST\">";
-    echo "<input name=\"scenariofile\" type=\"file\" />";
-    echo "<input type=\"submit\" value=\"upload\" />";
-    echo "</form>";
-    echo "</div>";
-} else {
-    echo "<div>";
-    echo "<form enctype=\"multipart/form-data\" action=\"\" method=\"POST\">";
-    echo "<input name=\"scenariofile\" type=\"file\" />";
-    echo "<input type=\"submit\" value=\"upload\" />";
-    echo "</form>";
-    echo "</div>";   
-}
-?>
+        ?>
 
-</body>
+        <div>;
+            <form enctype="multipart/form-data" action="" method="POST">;
+                <input name="scenariofile" type="file" />;
+                <input type="submit" value="upload" />";
+            </form>;
+        </div>;   
+
+    </body>
 </html>

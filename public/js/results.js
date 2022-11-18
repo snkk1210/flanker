7206714ba543f8c2d86b680a4114c0f993f4c34b
snkk1210 <n41210guitar@gmail.com>
2022-11-18 13:56:29 +0900

    modify

--- public/js/results.js
+++ public/js/results.js
@@ -1,7 +1,12 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const textareaEls = document.querySelectorAll(".results");
-  
-    textareaEls.forEach((textareaEl) => {
-      textareaEl.setAttribute("style", `height: ${textareaEl.scrollHeight}px;`);
-    });
+  const textareaEls = document.querySelectorAll(".results");
+
+  textareaEls.forEach((textareaEl) => {
+    textareaEl.setAttribute("style", `height: ${textareaEl.scrollHeight}px;`);
+  });
+});
+
+history.pushState(null, null, location.href);
+window.addEventListener('popstate', (e) => {
+  history.go(1);
 });

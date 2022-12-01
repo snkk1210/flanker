553c288d0a6e6b278def909b44f85cd2f2b09c40
snkk1210 <n41210guitar@gmail.com>
2022-12-02 00:17:48 +0900

    modify

--- public/js/loading.js
+++ public/js/loading.js
@@ -1,7 +1,20 @@
 document.getElementById("spinner").style.display ="none";
-function loading(e){
+const url = "api.php";
+
+function callApi(url) {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json_data) {
+      console.log(json_data);
+    });
+}
+
+function loading(e) {
 	const spinner = document.getElementById("spinner");
     const run = document.getElementById("run");
     spinner.style.display ="block";
     run.style.display ="none";
+    //setInterval("callApi(url)", 1000);
  };

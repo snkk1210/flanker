62dd44ec92054fb83c639654f9ba2a7bca5b6db8
snkk1210 <n41210guitar@gmail.com>
2023-05-18 02:40:09 +0900

    modify

--- public/result-list.php
+++ public/result-list.php
@@ -1,9 +1,24 @@
-<?php
-require('../lib/Common.php');
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Flanker</title>
+        <link rel="stylesheet" href="style.css?<?php echo date('YmdHis'); ?>" type="text/css">
+    </head>
+    <body>
+        <div>
+            <h1 class="title"><a href="/" rel="home">Flanker</a></h1>
+        </div>
+        <?php
+            require('../lib/Common.php');
 
-$root_directory = '/var/www/html';
-$directories = Common::getDirectories($root_directory);
+            $root_directory = '/var/www/html';
+            $directories = Common::getDirectories($root_directory);
 
-foreach ($directories as $directory) {
-    echo $directory . "<br>";
-}
+            foreach ($directories as $directory) {
+                echo $directory . "<br>";
+            }
+        ?>
+    </body>
+</html>

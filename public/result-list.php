74cf4a22a7e467e6abf5aa0ed942ae37773fa512
snkk1210 <n41210guitar@gmail.com>
2023-05-18 03:31:54 +0900

    modify

--- public/result-list.php
+++ public/result-list.php
@@ -12,12 +12,15 @@
         </div>
         <?php
             require('../lib/Common.php');
+            $request_host = $_SERVER['SERVER_ADDR'];
 
             $root_directory = '/var/www/html';
             $directories = Common::getDirectories($root_directory);
 
             foreach ($directories as $directory) {
-                echo str_replace($root_directory, '', $directory) . "<br>";
+                $result_dir = str_replace($root_directory, '', $directory);
+                $result_url = "http://" . $request_host . "/" . $result_dir;
+                echo '<a href="' . $result_url . '" rel="home">' . str_replace($root_directory, '', $directory) . '</a>' . "<br>";
             }
         ?>
     </body>

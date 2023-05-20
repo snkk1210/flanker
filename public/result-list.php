1e86abff9800497a549df74d4cd2a5732c271985
snkk1210 <n41210guitar@gmail.com>
2023-05-20 22:37:51 +0900

    modify: add table

--- public/result-list.php
+++ public/result-list.php
@@ -17,16 +17,21 @@
             $root_directory = '/var/www/html';
             $directories = Common::getDirectories($root_directory);
 
-            foreach ($directories as $directory) {
+            foreach ($directories as $key => $directory) {
                 $result_dir = str_replace($root_directory, '', $directory);
                 $result_url = "http://" . $request_host . "/" . $result_dir;
 
                 $slash_count = substr_count($result_dir, '/');
 
                 if ($slash_count >= 2) {
-                    echo '<a href="' . $result_url . '" target="_blank" rel="noopener noreferrer">' . $result_dir . '</a>' . "<br>";
+                    echo '<tr><td><a href="' . $result_url . '" target="_blank" rel="noopener noreferrer">' . $result_dir . '</a></td></tr>';
                 } else {
-                    echo '<strong><a href="' . $result_url . '" target="_blank" rel="noopener noreferrer">' . str_replace('/', '# ', $result_dir) . '</a>' . "<br></strong>";
+                    if ($key != 0) {
+                        echo "</table>";
+                        echo "<br>";
+                    }
+                    echo '<strong><a href="' . $result_url . '" target="_blank" rel="noopener noreferrer">' . str_replace('/', '# ', $result_dir) . '</a></strong>';
+                    echo "<table border='1'>";
                 }
             }
         ?>

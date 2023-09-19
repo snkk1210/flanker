c9689b0fff6932aad6e4d4eff8fc870d9519ec23
snkk1210 <n41210guitar@gmail.com>
2023-09-19 18:00:05 +0900

    add: logger class

---
+++ lib/Logger.php
@@ -0,0 +1,38 @@
+<?php
+
+class Logger
+{
+    /**
+     * NOTE: 
+     */
+    public static function error($message)
+    {
+        //
+    }
+
+    /**
+     * NOTE: 
+     */
+    public static function warn($message)
+    {
+        //
+    } 
+
+    /**
+     * NOTE: 
+     */
+    public static function info($message)
+    {
+        //
+    }
+
+    /**
+     * NOTE: 
+     */
+    public static function debug($message)
+    {
+        //
+    }
+}
+
+?>
